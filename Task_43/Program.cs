d288e43cfe4dc862ec9f5a9e0fb0c1552b613e1b
alexey.rotar <ar_mail@inbox.ru>
2023-02-01 21:36:23 +0500

    v2

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -1,18 +1,22 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // Метод вычисления координат пересечения
-(float, float) GetCrossPoint (float k1, float b1, float k2, float b2)
+(float, float, bool) GetCrossPoint (float k1, float b1, float k2, float b2)
 {
     float x=0;
     float y=0;
+    bool parallel=false;
     if (!(k1==k2))
     {
         x = (b2-b1)/(k1-k2);
         y = k1*x+b1;
     }
     else
-        Console.WriteLine($"Линии параллельны");
-    return (x, y);    
+    {
+        Console.WriteLine($"Линии параллельны. Точки пересечения нет.");
+        parallel=true;
+    }
+    return (x, y, parallel);    
 }
 
 // Интерфейс
@@ -49,9 +53,11 @@ while (!float.TryParse(Console.ReadLine(), out b2))
 }
 
 
-(float x, float y) = GetCrossPoint(k1, b1, k2, b2);
+(float x, float y, bool parallel) = GetCrossPoint(k1, b1, k2, b2);
 
 Console.WriteLine("Полученные уравнения:");        
 Console.WriteLine($"y = {k1}x + {b1};");
-Console.WriteLine($"y = {k2}x + {b2};"); 
+Console.WriteLine($"y = {k2}x + {b2};");
+
+if (!parallel) 
 Console.WriteLine($"Координаты точки пересечения: ({x}, {y})");
