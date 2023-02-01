77178cde15f17f2ece642703ef22596f09474b3f
alexey.rotar <ar_mail@inbox.ru>
2023-02-01 21:29:27 +0500

    v1

---
+++ Task_41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Создание массива
+float[] CreatingArray(int size)
+{
+    float[] array = new float[size];
+    return array;
+}
+
+// Заполонение значениями
+float[] FillingArray(float[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        float value;
+        while (!float.TryParse(Console.ReadLine(), out value))
+        {
+            Console.WriteLine("Некорректное значение!");             
+        }  
+        array[i] = value;
+    }
+    return array;
+}
+
+// Метод посчета значений > 0
+int GetAboveZeroValues(float[] array)
+{
+    int zeroCount = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i] > 0)
+            zeroCount++;        
+    }
+    return zeroCount;    
+}
+
+// Интерфейс
+Console.WriteLine("Сколько значений будет введено?");
+int size;
+while (!int.TryParse(Console.ReadLine(), out size) || size==0)
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+Console.WriteLine("Введите значения:");
+
+Console.WriteLine($"Значений больше нуля: {GetAboveZeroValues(FillingArray(CreatingArray(size)))}");
---
+++ Task_43/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Метод вычисления координат пересечения
+(float, float) GetCrossPoint (float k1, float b1, float k2, float b2)
+{
+    float x=0;
+    float y=0;
+    if (!(k1==k2))
+    {
+        x = (b2-b1)/(k1-k2);
+        y = k1*x+b1;
+    }
+    else
+        Console.WriteLine($"Линии параллельны");
+    return (x, y);    
+}
+
+// Интерфейс
+Console.WriteLine("y = k1*x + b1;");
+Console.WriteLine("y = k2*x + b2;"); 
+Console.WriteLine("Введите коэффициенты:");
+
+Console.Write("k1: ");
+float k1;
+while (!float.TryParse(Console.ReadLine(), out k1))
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+
+Console.Write("b1: ");
+float b1;
+while (!float.TryParse(Console.ReadLine(), out b1))
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+
+Console.Write("k2: ");
+float k2;
+while (!float.TryParse(Console.ReadLine(), out k2))
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+
+Console.Write("b2: ");
+float b2;
+while (!float.TryParse(Console.ReadLine(), out b2))
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+
+
+(float x, float y) = GetCrossPoint(k1, b1, k2, b2);
+
+Console.WriteLine("Полученные уравнения:");        
+Console.WriteLine($"y = {k1}x + {b1};");
+Console.WriteLine($"y = {k2}x + {b2};"); 
+Console.WriteLine($"Координаты точки пересечения: ({x}, {y})");
